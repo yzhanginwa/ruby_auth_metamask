d73f17baa8de571632d76af898d0c15ca23fbc22
Ethan Zhang <yzhang.wa@gmail.com>
2023-12-16 00:58:27 +0800

    added named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RubyAuthMetamask::Engine.routes.draw do
-  get  'signin', to: 'users#signin'
-  post 'signin', to: 'users#verify'
+  get  'signin', to: 'users#signin', as: 'signin'
+  post 'verify', to: 'users#verify', as: 'verify'
 
   root           to: 'users#signin'
 end
