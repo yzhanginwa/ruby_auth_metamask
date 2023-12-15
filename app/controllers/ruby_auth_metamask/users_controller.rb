c3434cab8ed30fc9ec553d77230be1895c112b4c
Ethan Zhang <yzhang.wa@gmail.com>
2023-12-15 11:38:24 +0800

    implemented the signin page

--- app/controllers/ruby_auth_metamask/users_controller.rb
+++ app/controllers/ruby_auth_metamask/users_controller.rb
@@ -1,6 +1,8 @@
 module RubyAuthMetamask
   class UsersController < ApplicationController
     def signin
+      @message = "ruby_auth_metamask:#{SecureRandom.hex}"
+      session[:message] = @message
     end
 
     def verify
